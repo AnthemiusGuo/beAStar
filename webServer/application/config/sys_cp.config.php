087adf7b1e2c5e011d4b48bf7722afdf0adc0a8f
Anthemius Guo <NJ.weihang@gmail.com>
2015-07-09 11:16:05 +0800

    ss

---
+++ webServer/application/config/sys_cp.config.php
@@ -0,0 +1,7 @@
+<?php
+// |	0 = Disables logging, Error logging TURNED OFF
+// |	1 = Error Messages (including PHP errors)
+// |	2 = Debug Messages
+// |	3 = Informational Messages
+// |	4 = All Messages
+$app_config['log_threshold'] = 1;
